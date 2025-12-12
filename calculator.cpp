46ec0a165443486206cfef6ba8dc573208d65f73
chemistcoder90 <motaher.emon360@gmail.com>
2025-12-12 16:41:05 +0600

    solved a bug. //+---+ previous one only choosed  last odd position as operator.

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,27 @@ using namespace std;
 #include<stdio.h>
 
 int k=0, result=0, esc=0, a,b;
+char ch, chLast;
+
+char ScanString(){ 				//help of gpt
+	string s;
+	while(true){
+		cin>>ch;
+		if(ch=='q' || ch=='r') return ch;
+		if(isdigit(ch)) break;
+		else s.push_back(ch);
+	}
+	int Ssize=s.size();
+	for(int i=Ssize-1; i>=0; i--){
+		if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/' || s[i]=='q' || s[i]=='r') {
+			chLast=s[i];
+			break;
+		}
+	}
+	
+	return chLast;
+
+}
 
 void menu(){
 	printf("You can perform: Addition\t Subtraction\tMultiplication\tDivision\n");
@@ -12,43 +33,36 @@ void menu(){
 	printf("Format: 2 + 3-...<enter>\n");
 }
 void add(){
-	if (scanf(" %d", &b) != 1) {
-    return;
-	}
+	cin.unget();
+	scanf(" %d", &b);
 	int a=result;
 	result+=b;
-	printf("%d + %d = %d\n",a,b, result);
+	printf("Total= %d + %d = %d\n",a,b, result);
 }
 void sub(){
-  int b;
-  if (scanf(" %d", &b) != 1) {
-     return;
-	}
+	cin.unget();
+	scanf(" %d", &b);
 	int a=result;
 	result-=b;
-	printf("%d - %d = %d\n",a,b, result);
+	printf("Total= %d - %d = %d\n",a,b, result);
 }
 void mul(){
-  int b;
-  if (scanf(" %d", &b) != 1) {
-     return;
-	}
+	cin.unget();
+  	scanf(" %d", &b);
 	int a=result;
 	result*=b;
-	printf("%d * %d = %d\n",a,b, result);
+	printf("Total= %d * %d = %d\n",a,b, result);
 }
 void div(){
-  int b;
-  if (scanf(" %d", &b) != 1) {
-     return;
-	}
+	cin.unget();
+    scanf(" %d", &b);
 	while(b==0){
 		printf("You can't divide with 0. Choose Integer value other than 0.\n");
 		scanf(" %d", &b);
 	}
 	int a=result;
 	result/=b;
-	printf("%d / %d = %d\n",a,b, result);
+	printf("Total= %d / %d = %d\n",a,b, result);
 }
 int main(){
 	menu();
@@ -58,14 +72,14 @@ int main(){
 			esc=1;
 			result=a;
 		}
-		char op;
-		scanf(" %c", &op);
+		char op=ScanString();
 		switch(op){
 			case '+': add(); break;
 			case '-': sub(); break;
 			case '*': mul(); break;
 			case '/': div(); break;
-			case 'q': return 0;
+			case 'q': printf("Quiting... Have a Nice Day:)\n");
+					  return 0;
 			case 'r': result=0;
 					 esc=0;
 					 system("clear");
